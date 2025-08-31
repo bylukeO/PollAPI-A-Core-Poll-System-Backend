f0328a1c86aa66e1e6dd822de4d7a360eca7e1c9
Luke Oladejo <bylukeo@gmail.com>
2025-08-31 14:44:04 +0100

    feat: Complete core API functionality with vote endpoint - Implemented the POST endpoint to record votes. - Finalized all serializers and views for full CRUD functionality.

--- polls/urls.py
+++ polls/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('polls/', views.PollList.as_view(), name='poll-list'),
     path('polls/<int:pk>/', views.PollDetail.as_view(), name='poll-detail'),
     path('polls/<int:pk>/vote/', views.VoteCreate.as_view(), name='poll-vote'),
+    path('polls/<int:poll_id>/options/', views.OptionList.as_view(), name='poll-options'),
+    path('options/', views.OptionList.as_view(), name='option-list'),
+    path('options/<int:pk>/', views.OptionDetail.as_view(), name='option-detail'),
     path('votes/', views.VoteList.as_view(), name='vote-list'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 # polls/views.py
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.exceptions import ValidationError
+from django.shortcuts import get_object_or_404
 from .models import Poll, Option, Vote
 from .serializers import PollSerializer, OptionSerializer, VoteSerializer
 
@@ -18,8 +19,33 @@ class VoteCreate(generics.CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         poll_id = self.kwargs['pk']
+        
+        # Validate poll exists
+        poll = get_object_or_404(Poll, pk=poll_id)
+        
         data = request.data.copy()
         data['poll'] = poll_id
+        
+        # Validate option_id is provided
+        if 'option_id' not in data or not data['option_id']:
+            return Response(
+                {'error': 'option_id is required'}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+        # Validate option exists and belongs to poll
+        try:
+            option = Option.objects.get(pk=data['option_id'])
+            if option.poll.pk != poll.pk:
+                return Response(
+                    {'error': 'Option does not belong to this poll'}, 
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        except Option.DoesNotExist:
+            return Response(
+                {'error': 'Option does not exist'}, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
         
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -31,3 +57,23 @@ class VoteCreate(generics.CreateAPIView):
 class VoteList(generics.ListAPIView):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
+
+class OptionList(generics.ListCreateAPIView):
+    serializer_class = OptionSerializer
+    
+    def get_queryset(self):
+        poll_id = self.kwargs.get('poll_id')
+        if poll_id:
+            return Option.objects.filter(poll=poll_id)
+        return Option.objects.all()
+    
+    def perform_create(self, serializer):
+        poll_id = self.kwargs.get('poll_id')
+        if poll_id:
+            serializer.save(poll_id=poll_id)
+        else:
+            serializer.save()
+
+class OptionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Option.objects.all()
+    serializer_class = OptionSerializer
